755dc920aa26990c72a02b635cbad9e63d4a5367
SYEDMUHAMMADFAHAD1 <161588586+SYEDMUHAMMADFAHAD1@users.noreply.github.com>
2024-04-04 09:21:23 +0500

    adding

---
+++ main.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+(async () => {
+    const answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "num1",
+            message: chalk.redBright("Enter your First number"),
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: chalk.redBright("Enter your Second number"),
+        },
+        {
+            type: "list",
+            name: "operator",
+            message: chalk.green("Enter your Operator"),
+            choices: ["+", "-", "*", "/"], // "Choices" should be "choices"
+        },
+    ]);
+    // Step 02
+    const { num1, num2, operator } = answer;
+    let result;
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        default:
+            throw new Error("Invalid operator"); // invalid operator case
+    }
+    console.log(`${num1} ${operator} ${num2} = ${result}`);
+})();
